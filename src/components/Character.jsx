426664e50af13f99bcbe87923fc1e26769eb5d5a
horex-pro <hosseinhorex@gmail.com>
2023-09-19 23:59:25 +0330

    test

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,3 +1,5 @@
+import { EyeIcon } from "@heroicons/react/24/outline";
+
 function Character ({item})
 {
     return (
@@ -5,14 +7,18 @@ function Character ({item})
             <img src={ item.image } alt={ item.name } />
             <h3 className="name">
                 <span>{ item.gender === 'Male' ? '👨' : '👱‍♀️' }</span>
-                <span>{item.name }</span>
+                <span>{ item.name }</span>
             </h3>
             <div className="list-item__info info">
-                <span className={`status ${item.status === 'Dead' ? 'red' : ''}`}>
+                <span className={ `status ${ item.status === 'Dead' ? 'red' : '' }` }>
                 </span>
                 <span>{ item.status }</span>
-                <span>-{ item.species}</span>
+                <span>-{ item.species }</span>
+                
             </div>
+            <button className="icon red">
+                <EyeIcon />
+            </button>
         </div>
     )
 }
--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,9 +1,59 @@
+import { character, episodes } from "../../data/data";
+import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
 
 
 function CharacterDetail() {
-  return (
-    <div>CharacterDetail</div>
-  )
+    return (
+        <div style={ { flex: 1 } }>
+            <div className="character-detail">
+                <img src={ character.image } alt={ character.name } className="character-detail__img" />
+                <div className="character-detail__info">
+                    <h3 className="name">
+                        <span>
+                            { character.gender === 'Male' ? '👨 ' : '👱‍♀️ ' }
+                        </span>
+                        <span>
+                            {character.name }
+                        </span>
+                    </h3>
+                    <div className="info">
+                        <span className={ `status ${ character.status === 'Dead' ? 'red' : '' }` }></span>
+                        <span>&nbsp;{ character.status }</span>
+                        <span>&nbsp;{ character.species}</span>
+                    </div>
+                    <div className="location">
+                        <p>Last known location:</p>
+                        <p>{ character.location.name}</p>
+                    </div>
+                    <div className="actions">
+                        <button className="btn btn--primary">
+                            add to favourite
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="character-episodes">
+                <div className="title">
+                    <h2>List of Episodes</h2>
+                    <button>
+                        <ArrowUpCircleIcon className="icon"/>
+                    </button>
+                </div>
+                <ul>
+                    {
+                        episodes.map(( item , index) => <li key={ item.id }>
+                            <div>
+                                {String(index + 1).padStart(2,'0')} &nbsp; { item.episode } : <strong>{ item.name}</strong>
+                            </div>
+                            <div className="badge badge--secondary">
+                                {item.air_date}
+                            </div>
+                        </li>)
+                    }
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default CharacterDetail
